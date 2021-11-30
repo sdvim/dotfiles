85aa5d8a1ecf7ad97719ce622de7ee87a67893a1
Steve Della Valentina <s.dellavalentina@gmail.com>
2021-11-30 15:22:43 -0700

    Add Amethyst settings

--- .config/setup.sh
+++ .config/setup.sh
@@ -12,6 +12,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/mathiasbynens/dotfiles/mai
 # Set iterm2 preferences folder to read from .config
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2"
 
+# Make a symlink for Amethyst settings
+ln -s ~/.config/amethyst/com.amethyst.Amethyst.plist ~/Library/Preferences/com.amethyst.Amethyst.plist
+
 # Make a symlink for VS Code settings
 ln -s ~/.config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
